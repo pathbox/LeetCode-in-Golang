b2d119e557dc3b8d5763214c1cd9681de777db11
pathbox <cary.luo@ucloud.cn>
2019-01-07 09:48:53 +0800

    009.palindrome-number

---
+++ 009.palindrome-number/README.md
@@ -0,0 +1,9 @@
+# [9. Palindrome Number](https://leetcode.com/problems/palindrome-number/)
+
+## 题目
+Determine whether an integer is a palindrome. Do this without extra space.
+
+## 解题思路
+检查一个整数是否是回文。
+
+先把整数转换成字符串，两个索引i，j，i从头开始走，j从尾开始走，比较索引对应的两个字符是否相同，不相同则不是回文，返回false。
---
+++ 009.palindrome-number/palindrome-number.go
@@ -0,0 +1,19 @@
+package Problem009
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+	if x < 0 { // 负数肯定不是
+		return false
+	}
+
+	s := strconv.Itoa(x)
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
+}
