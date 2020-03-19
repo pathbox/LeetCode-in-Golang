801ee5416119c9ee06a598d1110566f18f0c0c16
pathbox <cary.luo@ucloud.cn>
2020-03-19 23:43:03 +0800

    141.环形链表example2

--- 141.环形链表/141_example1.go
+++ 141.环形链表/141_example1.go
@@ -7,6 +7,7 @@ type ListNode struct {
 
 // 开一个哈希表记录该节点是否已经遍历过，值记录节点索引
 // 该节点遍历过，形成了环
+// 将遍历过的节点记录下来，如果又遍历到了，表示链表有环，时间复杂度O(n)，空间复杂度O(n)
 func hasCycle(head *ListNode) bool {
 	hash := make(map[*ListNode]struct{})
 	for head != nil {
