ca70072e613acb146f2c90f9648f5391391ebbce
pathbox <cary.luo@ucloud.cn>
2020-03-21 22:53:55 +0800

    026.删除排序数组中的重复项example1

--- 026.删除排序数组中的重复项/026_example1.go
+++ 026.删除排序数组中的重复项/026_example1.go
@@ -1,9 +1,10 @@
 package LeetCode026
 
+// 快慢索引
 func removeDuplicates(nums []int) int {
 	low := 0
 	for fast := 0; fast < len(nums); fast++ {
-		if nums[low] != nums[fast] {
+		if nums[low] != nums[fast] { // 如果不相等，其实两个索引都会往后移动，如果相等，则慢索引保持不动，快索引继续往后移动
 			nums[low+1] = nums[fast]
 			low++
 		}
