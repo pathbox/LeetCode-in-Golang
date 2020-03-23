5aa673efd417d27880b1b4f76f50f91349f10113
pathbox <cary.luo@ucloud.cn>
2020-03-23 09:42:07 +0800

    108.将有序数组转为二叉搜索树example1

---
+++ 108.将有序数组转为二叉搜索树/108_example1.go
@@ -0,0 +1,20 @@
+package LeetCode108
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	left := nums[:mid]
+	right := nums[mid+1:]
+
+	node := &TreeNode{nums[mid], sortedArrayToBST(left), sortedArrayToBST(right)}
+	return node
+}
