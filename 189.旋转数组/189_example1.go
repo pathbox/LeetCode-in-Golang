941b0f8b6d665fec71ab190d124633030395e152
pathbox <cary.luo@ucloud.cn>
2020-03-23 23:28:08 +0800

    189.旋转数组example1

--- 189.旋转数组/189_example1.go
+++ 189.旋转数组/189_example1.go
@@ -3,5 +3,5 @@ package LeetCode189
 func rotate(nums []int, k int) {
 	k %= len(nums) // 是个环移动 所以用%取余
 	ans := append(nums[len(nums)-k:], nums[:len(nums)-k]...)
-	nums = append(nums[:0], ans...)
+	nums = append(nums[:0], ans...) // 赋值回给nums
 }
