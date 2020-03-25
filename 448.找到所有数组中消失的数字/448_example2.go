106a0d777f1ef85b4e25ed120929908ed9309d73
pathbox <cary.luo@ucloud.cn>
2020-03-25 22:53:18 +0800

    448 一个萝卜一个坑，如果这个坑没有被标记过，说明这个坑没被用过，也就是本来在这个坑上的数没有在数组中出现过，也就是消逝的数

--- 448.找到所有数组中消失的数字/448_example2.go
+++ 448.找到所有数组中消失的数字/448_example2.go
@@ -6,7 +6,7 @@ func findDisappearedNumbers(nums []int) []int {
 		if value < 0 {
 			value = -value // 把小于0的值转为大于0
 		}
-		if nums[value-1] > 0 { // value-1为index进行判断
+		if nums[value-1] > 0 { // value-1为index进行判断,索引值从0开始
 			nums[value-1] = -nums[value-1]
 		}
 	}
@@ -18,3 +18,5 @@ func findDisappearedNumbers(nums []int) []int {
 	}
 	return result
 }
+
+// 一个萝卜一个坑，如果这个坑没有被标记过，说明这个坑没被用过，也就是本来在这个坑上的数没有在数组中出现过，也就是消逝的数
