14ac8855baa4eacd282fa6efa14770776f4c5eac
pathbox <cary.luo@ucloud.cn>
2020-03-29 17:55:37 +0800

    820.单词的压缩编码example2

---
+++ 820.单词的压缩编码/820_example2.go
@@ -0,0 +1,54 @@
+package LeetCode820
+
+func minimumLengthEncoding(words []string) int {
+	var (
+		root = NewTree()
+	)
+	for i := 0; i < len(words); i++ {
+		temp := words[i]
+		insert(root, temp)
+	}
+	return findLeaf(root, 0)
+}
+
+type node struct {
+	isStr bool
+	next  []*node
+}
+
+func NewTree() *node {
+	return &node{
+		isStr: false,
+		next:  make([]*node, 26), // 只能用于存小写字母,每个索引位置标示对应的小写字母
+	}
+}
+
+func insert(root *node, str string) {
+	for i := len(str) - 1; i >= 0; i-- {
+		if root.next[str[i]-'a'] == nil {
+			root.next[str[i]-'a'] = &node{
+				isStr: i == 0,
+				next:  make([]*node, 26),
+			}
+		}
+		root = root.next[str[i]-'a']
+	}
+}
+
+func findLeaf(root *node, length int) int {
+	var (
+		isLeaf = true
+		sum    = 0
+	)
+	nodes := root.next
+	for i := 0; i < 26; i++ {
+		if nodes[i] != nil {
+			isLeaf = false
+			sum += findLeaf(nodes[i], length+1)
+		}
+	}
+	if isLeaf {
+		return length + 1
+	}
+	return sum
+}
