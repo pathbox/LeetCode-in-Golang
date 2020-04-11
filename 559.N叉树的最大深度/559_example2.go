e79dab9f22cd5cba82ff8d99f79ebaa1339ad856
pathbox <cary.luo@ucloud.cn>
2020-04-11 14:39:50 +0800

    每次进入一层，遍历这一层的所有节点数，将其Children节点入队列，将当前这层parent节点处队列，这一层level++ 然后继续对Children节点进行相同的操作，直到所有节点没有Children为止，说明是到了最长的叶子节点位置

--- 559.N叉树的最大深度/559_example2.go
+++ 559.N叉树的最大深度/559_example2.go
@@ -13,8 +13,8 @@ func maxDepth(root *Node) int {
 	var queue = []*Node{root}
 	var level int
 	for len(queue) > 0 {
-		length := len(queue)
-		for length > 0 { // 将当前这层的所有node的Children入queue
+		length := len(queue) // 当前层的节点数
+		for length > 0 {     // 将当前这层的所有node的Children入queue
 			length--
 			for _, n := range queue[0].Children {
 				queue = append(queue, n)
@@ -26,3 +26,6 @@ func maxDepth(root *Node) int {
 	}
 	return level
 }
+
+// 每次进入一层，遍历这一层的所有节点数，将其Children节点入队列，将当前这层parent节点处队列，这一层level++
+// 然后继续对Children节点进行相同的操作，直到所有节点没有Children为止，说明是到了最长的叶子节点位置
