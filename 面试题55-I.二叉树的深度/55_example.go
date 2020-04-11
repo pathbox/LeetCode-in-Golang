0add2032f6f7f75ade49b8eb4a4df5877fcec7bc
pathbox <cary.luo@ucloud.cn>
2020-04-11 21:34:33 +0800

    面试题55-I.二叉树的深度

---
+++ 面试题55-I.二叉树的深度/55_example.go
@@ -0,0 +1,41 @@
+package LeetCode700
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDepth(root.Left)
+	r := maxDepth(root.Right)
+
+	return max(l, r) + 1
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+/*
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+
+例如：
+
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+*/
