36407674bf0fae572630b4ecf2a93b1c240d9f50
pathbox <cary.luo@ucloud.cn>
2020-04-14 09:06:03 +0800

    098.验证二叉搜索树example3

---
+++ 098.验证二叉搜索树/098_example3.go
@@ -0,0 +1,31 @@
+package LeetCode098
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var last *TreeNode
+
+/*
+中序递归
+递归方向 左->根->右
+上一个节点 last 赋值时机
+last.Val 必须小于 root.Val （因为中序遍历=升序遍历）
+*/
+func isValidBST(root *TreeNode) bool {
+	last = &TreeNode{Val: -1 << 63}
+	return dfs(root)
+}
+
+func dfs(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if !dfs(root.Left) || root.Val <= last.Val {
+		return false
+	}
+	last = root
+	return dfs(root.Right)
+}
