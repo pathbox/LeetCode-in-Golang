65433d6ca2b676db9af2b018754db476a2504c7d
pathbox <cary.luo@ucloud.cn>
2020-04-20 23:15:06 +0800

    116.填充每个节点的下一个右侧节点指针example1

---
+++ 116.填充每个节点的下一个右侧节点指针/116_example1.go
@@ -0,0 +1,31 @@
+package LeetCode106
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	dfs(root)
+	return root
+}
+
+func dfs(root *Node) {
+	if root == nil {
+		return
+	}
+	left := root.Left
+	right := root.Right
+	for left != nil {
+		left.Next = right
+		left = left.Right
+		right = right.Left
+	}
+	//递归的调用左右节点，完成同样的纵深串联, 树的纵深遍历
+	dfs(root.Left)
+	dfs(root.Right)
+}
+
+// https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/solution/dong-hua-yan-shi-san-chong-shi-xian-116-tian-chong/
