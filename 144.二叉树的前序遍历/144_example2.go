13a861370182d423099f5c51eaa0ff0abac69c73
pathbox <cary.luo@ucloud.cn>
2020-04-22 09:08:36 +0800

    144.二叉树的前序遍历example3

--- 144.二叉树的前序遍历/144_example2.go
+++ 144.二叉树的前序遍历/144_example2.go
@@ -12,12 +12,12 @@ func preorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return res
 	}
-	stack := []*TreeNode{root}
+	stack := []*TreeNode{root} // 当前节点入栈
 	for len(stack) > 0 {
 		p := stack[len(stack)-1]
 		stack = stack[0 : len(stack)-1]
 		res = append(res, p.Val)
-		if p.Right != nil {
+		if p.Right != nil { // 先让右子树进栈，左子树后进栈，遍历的时候，就会先取左子树
 			stack = append(stack, p.Right)
 		}
 		if p.Left != nil {
