48e51e618f37fca0cd745843c8d7a563fe8b50eb
pathbox <cary.luo@ucloud.cn>
2020-05-06 23:18:01 +0800

    面试题0412求和路径example1

--- 814.二叉树剪枝/814_example1.go
+++ 814.二叉树剪枝/814_example1.go
@@ -13,7 +13,7 @@ func pruneTree(root *TreeNode) *TreeNode {
 
 	root.Left = pruneTree(root.Left)
 	root.Right = pruneTree(root.Right)
-	if root.Val == 0 && root.Left == nil && root.Right == nil {
+	if root.Val == 0 && root.Left == nil && root.Right == nil { // 等于0 且当前节点是叶子节点，返回nil剪掉
 		return nil
 	}
 	return root
---
+++ 面试题0412求和路径/0412_examplw.go
@@ -0,0 +1,26 @@
+package LeetCode103
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) int {
+	var f func(*TreeNode, []int) int
+	f = func(r *TreeNode, a []int) (result int) {
+		if r != nil {
+			s := make([]int, len(a)+1)
+			copy(s, a)
+			for i := range s {
+				s[i] += r.Val
+				if s[i] == sum {
+					result++
+				}
+			}
+			result += f(r.Left, s) + f(r.Right, s)
+		}
+		return
+	}
+	return f(root, []int{})
+}
