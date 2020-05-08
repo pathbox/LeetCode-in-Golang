d6af093c8391bc1d7848aec2fc4d156cf4895f91
pathbox <cary.luo@ucloud.cn>
2020-05-08 22:40:18 +0800

    784.字母大小写全排列example3

---
+++ 784.字母大小写全排列/784_example3.go
@@ -0,0 +1,35 @@
+package LeetCode784
+
+import "strings"
+
+func letterCasePermutation(S string) []string {
+	var count uint
+	ans := []string{}
+	for i := 0; i < len(S); i++ {
+		if isLetter(S[i]) {
+			count++ //count为字母个数
+		}
+	}
+	for j := 0; j < (1 << count); j++ { //结果中一共有1 << count个字符串 2的count次方
+		var c uint
+		var tmp string
+		for _, v := range S {
+			if isLetter(byte(v)) {
+				if (j>>c)&1 == 1 { //按位判断该连接大写还是小写字母
+					tmp += strings.ToLower(string(v))
+				} else {
+					tmp += strings.ToUpper(string(v))
+				}
+				c++
+			} else {
+				tmp += string(v)
+			}
+		}
+		ans = append(ans, tmp)
+	}
+	return ans
+}
+
+func isLetter(c byte) bool { //判断是否为字母的函数，因为题目说了S仅由数字和字母组成，所以只要大于‘9’就是字母了
+	return c > '9'
+}
