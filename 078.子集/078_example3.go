c96ee7e93b6f98e8f0c05858856d907a5063e827
pathbox <cary.luo@ucloud.cn>
2020-05-10 22:50:42 +0800

    078.子集example3

---
+++ 078.子集/078_example3.go
@@ -0,0 +1,23 @@
+package LeetCode078
+
+var (
+	res [][]int
+)
+
+func subsets(nums []int) [][]int {
+	res = make([][]int, 0)
+	backtrack([]int{}, nums, 0)
+	return res
+}
+
+func backtrack(temp, nums []int, start int) {
+	tmp := make([]int, len(temp))
+	copy(tmp, temp)
+	res = append(res, tmp)
+
+	for i := 0; i < len(nums); i++ {
+		temp = append(temp, nums[i])
+		backtrack(temp, nums, i+1)
+		temp = temp[:len(temp)-1] // 通过删掉最后一个元素实现回溯， 才会有 回溯 1， 再回溯 2，最后回溯 3
+	}
+}
