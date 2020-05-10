934e93b53039f0820a236fd8b53f8ffcded32cb5
pathbox <cary.luo@ucloud.cn>
2020-05-10 17:18:17 +0800

    078.子集example1

---
+++ 078.子集/078_example1.go
@@ -0,0 +1,26 @@
+package LeetCode078
+
+func subsets(nums []int) [][]int {
+	result := make([][]int, 0)
+	item := make([]int, 0)
+
+	result = append(result, item)
+	generate(0, nums, &item, &result)
+	return result
+}
+
+func generate(i int, nums []int, item *[]int, result *[][]int) {
+	if i >= len(nums) {
+		return
+	}
+	*item = append(*item, nums[i])
+	temp := make([]int, len(*item))
+	for i, v := range *item {
+		temp[i] = v
+	}
+	*result = append(*result, temp)
+	generate(i+1, nums, item, result)
+	*item = (*item)[:len(*item)-1]
+	generate(i+1, nums, item, result)
+	return
+}
