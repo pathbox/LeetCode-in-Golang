91c5f2a693271ab3c07685419cae2ef2b81185ef
pathbox <cary.luo@ucloud.cn>
2020-05-20 09:58:23 +0800

    200.岛屿数量example1

--- 079.单词搜索/079_example1.go
+++ 079.单词搜索/079_example1.go
@@ -1,8 +1,8 @@
 package LeetCode079
 
 func exist(board [][]byte, word string) bool {
-	n := len(borad)    // 矩阵长度
-	m := len(borad[0]) // 举证宽度
+	n := len(board)    // 矩阵长度
+	m := len(board[0]) // 举证宽度
 
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
@@ -13,6 +13,7 @@ func exist(board [][]byte, word string) bool {
 	}
 	return false
 }
+
 // k 下一个字符索引
 func backtrace(board [][]byte, word string, i, j, k int) bool {
 	// 找到了就返回不用继续
@@ -24,38 +25,44 @@ func backtrace(board [][]byte, word string, i, j, k int) bool {
 		return false
 	}
 
-	if board[i][j] != word[k] {
+	if board[i][j] != word[k] { // 字符比较相等
 		return false
 	}
 
 	tmp := board[i][j]
 	// 重置它是为了回溯往回找的时候避免重复使用，干脆，如果找到它是对的，就直接把它置为 空
-		// 等结束了之后再重置回来
-		board[i][j] = byte('') // 避免重复使用
+	// 等结束了之后再重置回来
+	board[i][j] = byte(' ') // 避免重复使用
 
-		// 开始上下左右探测
-		// left
-		if backtrace(board,word,i-1,j,k+1) {
-			return true
-		}
-		// right
-		if backtrace(board,word,i+1,j,k+1) {
-			return true
-		}
-		// up
-		if backtrace(board, word, i, j-1, k+1) {
-			return true
-		}
-		// down
-		if backtrace(board, word, i, j+1, k+1) {
-			return true
-		}
-		board[i][j] = tmp //这次调用没有，将值还原
-		return false
+	// 开始上下左右探测
+	// left
+	if backtrace(board, word, i-1, j, k+1) {
+		return true
+	}
+	// right
+	if backtrace(board, word, i+1, j, k+1) {
+		return true
+	}
+	// up
+	if backtrace(board, word, i, j-1, k+1) {
+		return true
+	}
+	// down
+	if backtrace(board, word, i, j+1, k+1) {
+		return true
+	}
+	board[i][j] = tmp //这次调用没有，将值还原
+	return false
 
 }
 
 /*
 O(2^n)
 分析过程：每个单词的字母在矩阵中都有四个方向查找，因此时间复杂度 4^n ~= O(2^n)
+
+在进入DFS前，设置temp变量记录board[i][j]原来的值，然后把board[i][j]的值原地改为' '
+等从DFS出来后，再把board[i][j]改回原来的值
+这样就可以避免重复进入
+并且因为是原地修改，避免了辅助变量，开销较小
+
 */
---
+++ 200.岛屿数量/200_example1.go
@@ -0,0 +1,38 @@
+package LeetCode200
+
+func numIslands(grid [][]byte) int {
+	n := len(grid)
+	if n == 0 {
+		return 0
+	}
+	m := len(grid[0])
+	res := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '1' { // 1 表示有陆地， 每次代入i，j位置的陆地，以当前这块陆地出发，进行上下左右的探测是否仍然为陆地,探测过的陆地会被设置为0.如果这些陆地是连在一起的，那么实际上还是一块，下一次取 i，j的时候访问过的陆地就不会在访问，它已经和上一次访问的陆地连在一块
+				helper(i, j, grid) // 结束一次dfs表示，一块陆地探测完毕，所以结果res++
+				res++
+			}
+		}
+	}
+	return res
+}
+
+func helper(i, j int, grid [][]byte) {
+	grid[i][j] = '0' // i，j这块陆地已经访问过,以当前这块陆地出发，进行上下左右的探测是否仍然为陆地
+
+	if i+1 < len(grid) && grid[i+1][j] == '1' {
+		helper(i+1, j, grid)
+	}
+
+	if i-1 >= 0 && grid[i-1][j] == '1' {
+		helper(i-1, j, grid)
+	}
+
+	if j+1 < len(grid[i]) && grid[i][j+1] == '1' {
+		helper(i, j+1, grid)
+	}
+	if j-1 >= 0 && grid[i][j-1] == '1' {
+		helper(i, j-1, grid)
+	}
+}
