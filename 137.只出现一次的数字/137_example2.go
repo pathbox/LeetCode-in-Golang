cd3900cf1e71d43e83259ff2a3fa3e895e8a75f9
pathbox <cary.luo@ucloud.cn>
2020-05-24 13:50:36 +0800

    137.只出现一次的数字example2

---
+++ 137.只出现一次的数字/137_example2.go
@@ -0,0 +1,10 @@
+package LeetCode137
+
+func singleNumber(nums []int) int {
+	a, b := 0, 0
+	for _, num := range nums {
+		a = a ^ num & ^b // num先对b按位清除；再与a异或
+		b = b ^ num & ^a // num先对b按位清除；再与a异或
+	}
+	return a
+}
