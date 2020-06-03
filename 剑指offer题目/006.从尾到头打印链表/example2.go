a50e1e960a14064adebc415487f2a891023ff3d5
pathbox <cary.luo@ucloud.cn>
2020-06-03 23:34:40 +0800

    006.从尾到头打印链表example2:反转链表

---
+++ 剑指offer题目/006.从尾到头打印链表/example2.go
@@ -0,0 +1,28 @@
+
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 反转链表
+func reversePrint(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+
+	var newHead *ListNode // 新增一个新的头节点
+	res := []int{}
+	for head != nil { // 不断遍历head指针
+		node := head.Next   // 保存next节点
+		head.Next = newHead // 倒序连接
+		newHead = head      // 将newHead移到头部
+		head = node
+	}
+
+	for newHead != nil {
+		res = append(res, newHead.Val)
+		newHead = newHead.Next
+	}
+	return res
+}
