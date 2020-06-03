33816111ed2103e9080cb67762146f59a6d167c9
pathbox <cary.luo@ucloud.cn>
2020-06-03 22:51:54 +0800

    005.替换空格example3

---
+++ 剑指offer题目/005.替换空格/example3.go
@@ -0,0 +1,15 @@
+package Offer005
+
+import "strings"
+
+func replaceSpace(s string) string {
+	var res strings.Builder
+	for i := range s {
+		if s[i] == ' ' {
+			res.WriteString("%20")
+		} else {
+			res.WriteByte(s[i])
+		}
+	}
+	return res.String()
+}
