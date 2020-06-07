1c19fdf419b7c748dee8a02497e3ac3a093c5260
pathbox <cary.luo@ucloud.cn>
2020-06-07 09:57:40 +0800

    010-II.青蛙跳台阶问题example2

---
+++ 剑指offer题目/010-II.青蛙跳台阶问题/example2.go
@@ -0,0 +1,15 @@
+package Offer010
+
+func numWays(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+
+	a, b, temp := 1, 1, 0
+	for i := 2; i <= n; i++ {
+		temp = b
+		b = (a + b) % 1000000007
+		a = temp
+	}
+	return b
+}
