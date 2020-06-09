a417159ca5bc1daa461bfdb01b65dd79c19c1f04
pathbox <cary.luo@ucloud.cn>
2020-06-09 09:28:32 +0800

    018.删除链表的节点example1

---
+++ 剑指offer题目/018.删除链表的节点/example1.go
@@ -0,0 +1,21 @@
+package Offer018
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{Val: 0, Next: head} // 假头
+	first := dummyHead                         // 双指针1
+	second := dummyHead.Next                   // 双指针2
+	for second != nil {
+		if second.Val == val {
+			first.Next = second.Next
+			break
+		}
+		first = first.Next
+		second = second.Next
+	}
+	return dummyHead.Next
+}
