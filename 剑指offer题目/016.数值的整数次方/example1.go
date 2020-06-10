72dae6abf0a1218ca0ded98a1cd33cfce283cc86
pathbox <cary.luo@ucloud.cn>
2020-06-10 09:35:54 +0800

    016.数值的整数次方example1

---
+++ 剑指offer题目/016.数值的整数次方/example1.go
@@ -0,0 +1,22 @@
+package Offer016
+
+// 本题是经典的分治法，用递归（recursion）和迭代（iteration）都可以完成。
+func help(x float64, n int) float64 {
+	if n == 1 {
+		return x
+	} else if n&1 == 0 {
+		return help(x*x, n/2)
+	} else {
+		return x * help(x, n-1)
+	}
+}
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		return help(1.0/x, -n)
+	} else if n == 0 {
+		return 1
+	} else {
+		return help(x, n)
+	}
+}
