3860bcf0baf2608c190b98aad161531c9751f536
pathbox <cary.luo@ucloud.cn>
2020-06-11 23:02:02 +0800

    021.调整数组顺序使奇数位于偶数签名example2

---
+++ 剑指offer题目/021.调整数组顺序使奇数位于偶数签名/example2.go
@@ -0,0 +1,14 @@
+package Offer021
+
+func exchange(nums []int) []int {
+	i := 0
+	j := 0
+	for j < len(nums) {
+		if nums[j]%2 != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		j++
+	}
+	return nums
+}
