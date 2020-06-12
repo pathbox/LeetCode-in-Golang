87db94260405a1f7050c386673b7ae03874420e6
pathbox <cary.luo@ucloud.cn>
2020-06-12 09:48:45 +0800

    025.合并两个排序的链表

---
+++ 剑指offer题目/025.合并两个排序的链表/example1.go
@@ -0,0 +1,33 @@
+package Offer025
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	tmp := &ListNode{}
+	head := tmp // 要有一个不动的head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tmp.Next = l1
+			l1 = l1.Next
+		} else {
+			tmp.Next = l2
+			l2 = l2.Next
+		}
+		tmp = tmp.Next
+	}
+	if l1 == nil {
+		tmp.Next = l2
+	} else {
+		tmp.Next = l1
+	}
+	return head.Next
+}
