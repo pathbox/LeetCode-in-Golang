566069e0a3fd571e3ebcfc8639911772e3af89c6
pathbox <cary.luo@ucloud.cn>
2020-06-13 15:13:38 +0800

    026.树的子结构example1

---
+++ 剑指offer题目/026.树的子结构/example1.go
@@ -0,0 +1,30 @@
+package Offer026
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	if A == nil || B == nil {
+		return false
+	}
+
+	return isContain(A, B) || isSubStructure(A.Left, B) || isSubStructure(A.Right, B) // 这里看到区别了吧,当前节点与B进行开始前序遍历对比，要么A继续前序遍历，其当前节点的左右子树开始继续与B对比
+}
+
+func isContain(A *TreeNode, B *TreeNode) bool {
+	if B == nil {
+		return true
+	}
+	if A == nil {
+		return false
+	}
+
+	if A.Val != B.Val {
+		return false
+	}
+
+	return isContain(A.Left, B.Left) && isContain(A.Left, B.Left)
+}
