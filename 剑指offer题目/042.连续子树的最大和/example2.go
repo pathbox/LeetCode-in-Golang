13ca86aec03a1cdfc58ee5d6af74ee846400a5ff
pathbox <cary.luo@ucloud.cn>
2020-06-18 09:34:34 +0800

    042.连续子树的最大和example2

---
+++ 剑指offer题目/042.连续子树的最大和/example2.go
@@ -0,0 +1,31 @@
+package Offer042
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	// 若 dp[i-1] \leq 0dp[i−1]≤0 ，说明 dp[i - 1]dp[i−1] 对 dp[i]dp[i] 产生负贡献，即 dp[i-1] + nums[i]dp[i−1]+nums[i] 还不如 nums[i]nums[i] 本身大
+
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = dp[i-1] + nums[i]
+		} else {
+			dp[i] = nums[i]
+		}
+	}
+
+	return max(dp)
+}
+
+func max(dp []int) int {
+	max := dp[0]
+	for _, v := range dp {
+		if max < v {
+			max = v
+		}
+	}
+	return max
+}
