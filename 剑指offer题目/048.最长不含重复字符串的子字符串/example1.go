98f1a0c2ae6b9d2844f8b7d43cbb7896ffd47cb4
pathbox <cary.luo@ucloud.cn>
2020-06-26 21:59:41 +0800

    048.最长不含重复字符串的子字符串example1

---
+++ 剑指offer题目/048.最长不含重复字符串的子字符串/example1.go
@@ -0,0 +1,35 @@
+package Offer048
+
+// dp
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(s))
+	dp[0] = 1
+
+	memo := make(map[byte]int)
+	max := 1
+	memo[s[0]] = 0
+	for i, _ := range s {
+		if s[i] == s[i-1] {
+			dp[i] = 1
+		} else {
+			lastAppear := -1
+			if idx, ok := memo[s[i]]; ok {
+				lastAppear = idx
+			}
+			if i-lastAppear > dp[i-1] {
+				dp[i] = dp[i-1] + 1
+			} else {
+				dp[i] = i - lastAppear
+			}
+		}
+		memo[s[i]] = i
+		if max < dp[i] {
+			max = dp[i]
+		}
+	}
+	return max
+}
