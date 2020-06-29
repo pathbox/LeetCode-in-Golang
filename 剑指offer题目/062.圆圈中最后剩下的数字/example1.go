3bbffe9bd2a0e93e8c861c7bdda57e5849d06275
pathbox <cary.luo@ucloud.cn>
2020-06-29 10:26:47 +0800

    约瑟夫环:倒推法

--- 剑指offer题目/062.圆圈中最后剩下的数字/example1.go
+++ 剑指offer题目/062.圆圈中最后剩下的数字/example1.go
@@ -1,14 +1,25 @@
 package Offer062
 
+/* 最终剩下一个人时的安全位置肯定为0，反推安全位置在人数为n时的编号
+人数为1： 0
+人数为2： (0+m) % 2
+人数为3： ((0+m) % 2 + m) % 3
+人数为4： (((0+m) % 2 + m) % 3 + m) % 4
+........
+迭代推理到n就可以得出答案
+*/
+
 func lastRemaining(n int, m int) int {
-	var f int
+	var result int
 	for i := 2; i <= n; i++ {
-		f = (f + m) % i
+		result = (result + m) % i
 	}
-	return f
+	return result
 }
 
 /*
+https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/c-dao-tui-fa-mian-shi-ti-62-yuan-quan-zhong-zui-ho/
+
 本题的难点在于递推公式的推导或者说是理解。
 
 f(n,m) 表示序号为 0,1,...,n-1 的圈要一直淘汰第 m 个人最终剩下来的序号，这里序号和对应的值是一致的。
