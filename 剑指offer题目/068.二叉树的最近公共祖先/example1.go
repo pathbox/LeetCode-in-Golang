a28e56a51a296579e40de780d3e3c4b8ecb3bc58
pathbox <cary.luo@ucloud.cn>
2020-06-29 15:16:07 +0800

    068.二叉树的最近公共祖先

---
+++ 剑指offer题目/068.二叉树的最近公共祖先/example1.go
@@ -0,0 +1,25 @@
+package LeetCode068
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
