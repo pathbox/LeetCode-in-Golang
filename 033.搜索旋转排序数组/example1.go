d3eaa4e9d217954552802a8f1105ca2f307ecc06
pathbox <cary.luo@ucloud.cn>
2020-07-03 09:37:43 +0800

    033.搜索旋转排序数

---
+++ 033.搜索旋转排序数组/example1.go
@@ -0,0 +1,28 @@
+package LeetCode033
+
+func search(nums []int, target int) int {
+	lo, lh := 0, len(nums)-1
+
+	for lo <= lh {
+		mid := lo + (lo+lh)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		// 先根据 nums[mid] 与 nums[lo] 的关系判断 mid 是在左段还是右段
+		if nums[mid] >= nums[lo] {
+			if target >= nums[lo] && target < nums[mid] {
+				lh = mid - 1
+			} else {
+				lo = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[lh] {
+				lo = mid + 1
+			} else {
+				lh = mid - 1
+			}
+		}
+	}
+	return -1 // 没有找到匹配
+}
