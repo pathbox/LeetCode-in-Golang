a34ca71475a1d11a95ca7b504b0421ef648b81ef
pathbox <cary.luo@ucloud.cn>
2020-07-07 09:32:38 +0800

    059-II.螺旋矩阵example1

---
+++ 059-II.螺旋矩阵/example1.go
@@ -0,0 +1,39 @@
+package LeetCode059
+
+func generateMatrix(n int) [][]int {
+	result := make([][]int, n)
+	for i := range result {
+		result[i] = make([]int, n)
+	}
+	fill(result, 1, 0, n)
+	return result
+}
+
+func fill(r [][]int, start, row, n int) {
+	if start > n*n {
+		return
+	}
+	if start == n*n {
+		r[row][row] = start
+		return
+	}
+	for i := row; i < n-1-row; i++ {
+		r[row][i] = start
+		start++
+	}
+	for i := row; i < n-1-row; i++ {
+		row[i][n-1-row] = start
+		start++
+	}
+	for i := n - 1 - row; i > row; i-- {
+		r[n-1-row][i] = start
+		start++
+	}
+	for i := n - 1 - row; i > row; i-- {
+		r[i][row] = start
+		start++
+	}
+
+	fill(r, start, row+1, n)
+
+}
