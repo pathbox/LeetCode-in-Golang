372208aa1f97fb70bc6d09c940a08fa24c6ab2f6
pathbox <cary.luo@ucloud.cn>
2020-07-07 09:46:51 +0800

    059-II.螺旋矩阵

--- 059-II.螺旋矩阵/example1.go
+++ 059-II.螺旋矩阵/example1.go
@@ -1,39 +1,42 @@
 package LeetCode059
 
 func generateMatrix(n int) [][]int {
-	result := make([][]int, n)
-	for i := range result {
-		result[i] = make([]int, n)
+	result := [][]int{}
+	for i := 0; i < n; i++ {
+		result = append(result, make([]int, n))
 	}
-	fill(result, 1, 0, n)
+
+	size := n * n
+	a, b := 0, n-1 //左右边界
+	c, d := 0, n-1 //上下边界
+
+	idx := 1
+	for idx <= size {
+		// 向右走
+		for i := a; i <= b; i++ {
+			result[c][i] = idx
+			idx++
+		}
+		c++ // 上边界收缩
+		// 向下走
+		for i := c; i <= d; i++ {
+			result[i][b] = idx
+			idx++
+		}
+		b-- // 右边界收缩
+		// 向左走
+		for i := b; i >= a; i-- {
+			result[d][i] = idx
+			idx++
+		}
+		// 下边界收缩
+		d--
+		// 向上走
+		for i := d; i >= c; i-- {
+			result[i][a] = idx
+			idx++
+		}
+		a++ // 左边界收缩
+	}
 	return result
-}
-
-func fill(r [][]int, start, row, n int) {
-	if start > n*n {
-		return
-	}
-	if start == n*n {
-		r[row][row] = start
-		return
-	}
-	for i := row; i < n-1-row; i++ {
-		r[row][i] = start
-		start++
-	}
-	for i := row; i < n-1-row; i++ {
-		row[i][n-1-row] = start
-		start++
-	}
-	for i := n - 1 - row; i > row; i-- {
-		r[n-1-row][i] = start
-		start++
-	}
-	for i := n - 1 - row; i > row; i-- {
-		r[i][row] = start
-		start++
-	}
-
-	fill(r, start, row+1, n)
-
 }
