57137f629775e4bce05877a092e048eedb298530
pathbox <cary.luo@ucloud.cn>
2020-07-08 22:57:09 +0800

    122.买卖股票的最佳时机example2

---
+++ 122.买卖股票的最佳时机/example2.go
@@ -0,0 +1,22 @@
+package LeetCode122
+
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	dp := make([][2]int, len(prices))
+	dp[0][0] = 0
+	dp[0][1] = -prices[0] // 第一天买入股票
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])
+	}
+	return dp[len(prices)-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
