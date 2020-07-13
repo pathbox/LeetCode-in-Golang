edea612c8e8b9b569cff23c7a86a6f623fe7f5b8
pathbox <cary.luo@ucloud.cn>
2020-07-13 23:49:10 +0800

    148.排序链表example2

--- 148.排序链表/example2.go
+++ 148.排序链表/example2.go
@@ -14,3 +14,28 @@ func sortList(head *ListNode) *ListNode {
 	quickSort(head, nil)
 	return head
 }
+
+// 用于交换两个节点上的数值
+func swap(a, b *ListNode) {
+	a.Val, b.Val = b.Val, a.Val
+}
+
+// 快速排序的主体函数
+func quickSort(head, end *ListNode) {
+	if head == end || head.Next == end {
+		return // 头节点已经是结束节点或要排序只有一个头节点直接返回
+	}
+	pivot := head.Val             // 头节点的值作为pivot
+	slow, fast := head, head.Next // 定义快慢指针
+	for fast != end {             // 当快指针不等于结束指针时，不断执行以下操作
+		if fast.Val <= pivot { // 如果快指针小于pivot
+			slow = slow.Next // 不断向后移动慢指针
+			swap(slow, fast) // 交换快慢指针指向的节点值
+		}
+		fast = fast.Next // 快指针向后移动一位
+	}
+	// 循环结束后交换头节点和慢指针指向的值,把pivot放在大小两堆数值的中间
+	swap(head, slow)
+	quickSort(head, slow) // 递归处理pivot左右两边的链表
+	quickSort(slow.Next, end)
+}
