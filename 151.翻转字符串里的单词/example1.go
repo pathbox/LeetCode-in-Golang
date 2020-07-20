e9009c90063ad002c780177a02dd475cbc27b623
pathbox <cary.luo@ucloud.cn>
2020-07-20 23:13:14 +0800

    151.翻转字符串里的单词example1

---
+++ 151.翻转字符串里的单词/example1.go
@@ -0,0 +1,15 @@
+package LeetCode151
+
+import "strings"
+
+func reverseWords(s string) string {
+	list := strings.Split(s, " ")
+	var res []string
+	for i := len(list) - 1; i > 0; i-- {
+		if len(list[i]) > 0 {
+			res = append(res, list[i])
+		}
+	}
+	s = strings.Join(res, " ")
+	return s
+}
