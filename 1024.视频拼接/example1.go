91c8a9658e59efb2ba0eacddab81ca3711141a53
pathbox <cary.luo@ucloud.cn>
2020-07-22 21:43:34 +0800

    1024.视频拼接

---
+++ 1024.视频拼接/example1.go
@@ -0,0 +1,62 @@
+package LeetCode1024
+
+func videoStitching(clips [][]int, T int) int {
+	quickSort2D(clips)
+	if len(clips) == 0 {
+		return -1
+	}
+	result := genJumpRecord(clips, T)
+
+	return result
+}
+
+func genJumpRecord(diffJumps [][]int, T int) int {
+	result := 0
+	preEnd := 0
+	curIndex := 0
+	curStart := diffJumps[0][0]
+	maxEnd := preEnd
+	for curIndex < len(diffJumps) && curStart <= preEnd {
+		for curStart <= preEnd && curIndex < len(diffJumps) {
+			if diffJumps[curIndex][1] > maxEnd {
+				maxEnd = diffJumps[curIndex][1]
+			}
+			curIndex++
+			if curIndex < len(diffJumps) {
+				curStart = diffJumps[curIndex][0]
+			}
+		}
+		result++
+		preEnd = maxEnd
+		if maxEnd >= T {
+			return result
+		}
+	}
+	return -1
+}
+
+func quickSort2D(nums [][]int) {
+	if len(nums) == 0 {
+		return
+	}
+	left := 0
+	right := len(nums) - 1
+	pivot := nums[left]
+	for left < right {
+		for nums[right][0] >= pivot[0] && left < right {
+			right--
+		}
+		nums[left] = nums[right]
+		for nums[left][0] < pivot[0] && left < right {
+			left++
+		}
+		nums[right] = nums[left]
+	}
+	nums[left] = pivot
+	if left > 0 {
+		quickSort2D(nums[:left])
+	}
+	if len(nums) > left+1 {
+		quickSort2D(nums[left+1:])
+	}
+}
