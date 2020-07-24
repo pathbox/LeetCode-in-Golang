9843a9d5c312deafbb5b649d0adf7f1ad58097d1
pathbox <cary.luo@ucloud.cn>
2020-07-24 09:38:55 +0800

    394.字符串解码

---
+++ 394.字符串解码/example1.go
@@ -0,0 +1,40 @@
+package LeetCode394
+
+// 很好理解 数字一个栈，字母一个栈
+
+func decodeString(s string) string {
+	alphaStack := make([]byte, 0)
+	countStack := make([]int, 0)
+	alphaIndex := make([]int, 0)
+	var count int
+	for i := 0; i < len(s); i++ {
+		if s[i] >= '0' && s[i] <= '9' {
+			count = count * int(s[i]-'0')
+		} else if s[i] == '[' { // [ 后面肯定是字母
+			// 可以把当前的数字放入次数数组
+			countStack = append(countStack, count)
+			count = 0
+			alphaIndex = append(alphaIndex, len(alphaStack))
+		} else if s[i] == ']' { // 对当前字母段出站
+			// 获取当前字母段的次数 然后次数出栈
+			c := countStack[len(countStack)-1]
+			countStack = countStack[:len(countStack)-1]
+
+			// 获取当前字母段开始的索引 然后索引出栈
+			index := alphaIndex[len(alphaIndex)-1]
+			alphaIndex = alphaIndex[:len(alphaIndex)-1]
+
+			//获取当前的字母段
+			str := string(alphaStack[index:])
+			// 字母数组出站
+			alphaStack = alphaStack[:index]
+
+			for j := 0; j < c; j++ {
+				alphaStack = append(alphaStack, []byte(str)...)
+			}
+		} else {
+			alphaStack = append(alphaStack, s[i])
+		}
+	}
+	return string(alphaStack)
+}
