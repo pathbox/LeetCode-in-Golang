0ce722e432e40090c2748f648af2639cf6fca5cd
pathbox <cary.luo@ucloud.cn>
2020-07-24 09:53:25 +0800

    394.字符串解码example2

--- 394.字符串解码/example1.go
+++ 394.字符串解码/example1.go
@@ -9,11 +9,12 @@ func decodeString(s string) string {
 	var count int
 	for i := 0; i < len(s); i++ {
 		if s[i] >= '0' && s[i] <= '9' {
-			count = count * int(s[i]-'0')
+			count = count*10 + int(s[i]-'0')
 		} else if s[i] == '[' { // [ 后面肯定是字母
 			// 可以把当前的数字放入次数数组
 			countStack = append(countStack, count)
 			count = 0
+
 			alphaIndex = append(alphaIndex, len(alphaStack))
 		} else if s[i] == ']' { // 对当前字母段出站
 			// 获取当前字母段的次数 然后次数出栈
@@ -22,12 +23,12 @@ func decodeString(s string) string {
 
 			// 获取当前字母段开始的索引 然后索引出栈
 			index := alphaIndex[len(alphaIndex)-1]
-			alphaIndex = alphaIndex[:len(alphaIndex)-1]
+			alphaIndex = alphaIndex[:len(alphaIndex)-1] // 此时len(alphaIndex)-1之前的字母是已经排好的
 
 			//获取当前的字母段
 			str := string(alphaStack[index:])
 			// 字母数组出站
-			alphaStack = alphaStack[:index]
+			alphaStack = alphaStack[:index] // 会把字母出栈，要不会多计算一次
 
 			for j := 0; j < c; j++ {
 				alphaStack = append(alphaStack, []byte(str)...)
