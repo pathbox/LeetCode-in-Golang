62520851bb6829e7b319fd368b87aaf6117e4d06
pathbox <cary.luo@ucloud.cn>
2020-07-26 10:20:53 +0800

    256.粉刷房子

---
+++ 256.粉刷房子/example1.go
@@ -0,0 +1,27 @@
+package LeetCode256
+
+func minCost(costs [][]int) int {
+	if len(costs) == 0 {
+		return 0
+	}
+	var prev [3]int
+	var curr [3]int
+	for i := 0; i < 3; i++ { // 初始化第一个房子的三种颜色的cost
+		prev[i] = costs[0][i]
+	}
+
+	for i := 1; i < len(costs); i++ {
+		curr[0] = costs[i][0] + min(prev[1], prev[2])
+		curr[1] = costs[i][1] + min(prev[0], prev[1])
+		curr[2] = costs[i][2] + min(prev[0], prev[1])
+		prev = curr
+	}
+	return min(min(prev[0], prev[1]), prev[2]) // 返回prev中最小的值就是最小的cost
+
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
