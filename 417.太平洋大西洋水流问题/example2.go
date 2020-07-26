e0db62ffb5f96bc25b99dfcb204f48678da184bf
pathbox <cary.luo@ucloud.cn>
2020-07-26 13:19:45 +0800

    417.太平洋大西洋水流问题example2

--- 417.太平洋大西洋水流问题/example2.go
+++ 417.太平洋大西洋水流问题/example2.go
@@ -1,58 +1,83 @@
 package LeetCode417
 
 func pacificAtlantic(matrix [][]int) [][]int {
-	res := make([][]int, 0)
-	if nil == matrix || len(matrix) == 0 {
+
+	res := [][]int{}
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return res
 	}
+	row, col := len(matrix), len(matrix[0])
 
-	row, col, dirs := len(matrix), len(matrix[0]), [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-	// 是否在区域中
-	inArea := func(x, y int) bool {
-		return x >= 0 && x < row && y >= 0 && y < col
+	// 设置位
+	setBit := func(n, i int) int {
+		return n | (1 << i)
+	}
+	// 验证位
+	checkBit := func(n, i int) bool {
+		return (n&(1<<i) != 0)
+	}
+	// 太平洋的标记位
+	const Pacific = 0
+	// 大西洋的标记位
+	const Atlantic = 1
+	// 标记矩阵，如果 Pacific 位被设置，则此顶点可达太平洋，大西洋同理
+	m := make([][]int, row)
+	for i := 0; i < row; i++ {
+		m[i] = make([]int, col)
 	}
 
-	var dfs func(x, y int, matrix [][]int, travelled [][]int)
-	dfs = func(x, y int, matrix [][]int, travelled [][]int) {
-		if travelled[x][y] == 1 {
+	var dfs func(x, y, value, ocean int)
+	dfs = func(x, y, value, ocean int) {
+
+		// 递归结束条件一：y、x超过限制
+		if x >= row || y >= col {
 			return
 		}
-		travelled[x][y] = 1
-		// dirs是四个方向
-		for _, d := range dirs {
-			// 根据四个方向 得到下一个点
-			newx := x + d[0]
-			newy := y + d[1]
-			// 不在区域 下一个点的高度小于前一个点 已经访问过，都不满足条件，继续下一个循环
-			if !inArea(newx, newy) || matrix[x][y] > matrix[newx][newy] || travelled[newx][newy] == 1 {
-				continue
-			}
-			dfs(newx, newy, matrix, travelled)
+		if x < 0 || y < 0 {
+			return
 		}
-	}
 
-	pcf, atl := make([][]int, row), make([][]int, row) // 大西洋 太平洋 初始化
-	for i := 0; i < row; i++ {
-		pcf[i] = make([]int, col)
-		atl[i] = make([]int, col)
-	}
-	// 从外层边缘出发
-	for i := 0; i < row; i++ {
-		dfs(i, 0, matrix, pcf)
-		dfs(i, col-1, matrix, atl)
-	}
+		// 递归结束条件二：当前顶点已经被访问且设置了标记
+		// 此时就无需再从这个顶点进行递归了
+		if checkBit(m[x][y], ocean) {
+			return
+		}
 
-	for i := 0; i < col; i++ {
-		dfs(0, i, matrix, pcf)
-		dfs(row-1, i, matrix, atl)
-	}
+		if ocean == Pacific && (y == 0 || x == 0) {
+			// 太平洋的边界
+			m[x][y] = setBit(m[x][y], ocean)
+		} else if ocean == Atlantic && (y == col-1 || x == row-1) {
+			// 大西洋的边界
+			m[x][y] = setBit(m[x][y], ocean)
+		} else if matriy[x][y] >= value {
+			// 当前顶点能到达下一个顶点
+			m[x][y] = setBit(m[x][y], ocean)
+		} else {
+			// 递归结束条件三：当前顶点无法到达下一个顶点
+			return
+		}
 
+		// 上
+		dfs(x-1, y, matriy[x][y], ocean)
+		// 左
+		dfs(x, y-1, matriy[x][y], ocean)
+		// 下
+		dfs(x+1, y, matriy[x][y], ocean)
+		// 右
+		dfs(x, y+1, matriy[x][y], ocean)
+	}
+	// 查找太平洋
+	dfs(0, 0, -1, Pacific)
+	// 查找大西洋
+	dfs(row-1, col-1, -1, Atlantic)
+	// 找出既能到达太平洋，也能到达大西洋的顶点
 	for i := 0; i < row; i++ {
 		for j := 0; j < col; j++ {
-			if pcf[i][j] == 1 && atl[i][j] == 1 { // 需要两个都同时满足
-				res = append(res, [][]int{{i, j}}...)
+			if checkBit(m[i][j], Pacific) && checkBit(m[i][j], Atlantic) {
+				res = append(res, []int{i, j})
 			}
 		}
 	}
+
 	return res
 }
