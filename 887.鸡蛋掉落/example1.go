433e46a894a747c6940d586c772cd351b3c80bb3
pathbox <cary.luo@ucloud.cn>
2020-08-01 21:10:51 +0800

    887.鸡蛋掉落example1

---
+++ 887.鸡蛋掉落/example1.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// https://leetcode-cn.com/problems/super-egg-drop/solution/gobian-ma-jie-fa-shuang-1000ms-19mb-by-ilwwli/
+
+func superEggDrop(K int, N int) int {
+	N++
+	for base := 0; ; base++ {
+		maxN := 0
+		for i := 0; i <= K && i <= base; i++ {
+			maxN += comb(i, base)
+		}
+		if maxN >= N {
+			return base
+		}
+	}
+}
+
+func comb(m, n int) int {
+	if m > n {
+		m = n
+	}
+	if m > n/2 {
+		m = n - m
+	}
+	ans := 1
+	for i := n - m + 1; i <= n; i++ {
+		ans *= i
+	}
+	for i := 2; i <= m; i++ {
+		ans /= i
+	}
+	return ans
+}
+
+func main() {
+	r := superEggDrop(3, 14)
+	fmt.Println("count:", r)
+}
