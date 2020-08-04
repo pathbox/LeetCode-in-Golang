3f2d2332fbc3b23e7d689227744effcd493b9af5
pathbox <cary.luo@ucloud.cn>
2020-08-04 09:51:23 +0800

    069.x的平方根

---
+++ 069.x的平方根/example1.go
@@ -0,0 +1,17 @@
+package LeetCode069
+
+// 二分法不断找相近的整数
+func mySqrt(x int) int {
+	l, r := 0, x
+	ans := -1
+	for l <= x {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
+			ans = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ans
+}
