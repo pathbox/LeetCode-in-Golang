dc4296fd4228d091e701e51c63ad437003812e2c
pathbox <cary.luo@ucloud.cn>
2020-08-04 22:03:13 +0800

    075.颜色分类example1

---
+++ 075.颜色分类/example1.go
@@ -0,0 +1,17 @@
+package LeetCode075
+
+// 遍历数组 不断的将0 值交换到首部， 2值交换到尾部，中间的自然会是1
+func sortColors(nums []int) {
+	l, r := 0, len(nums)-1
+	for i := 0; i <= r; i++ {
+		if nums[i] == 0 {
+			nums[l], nums[i] = nums[i], nums[l]
+			l++
+		}
+		if nums[i] == 2 {
+			nums[r], nums[i] = nums[i], nums[r]
+			i-- // 这里要注意哦
+			r--
+		}
+	}
+}
