9e7b10ff58fce931600a7a0d383ec66fb7d8967f
pathbox <cary.luo@ucloud.cn>
2020-08-09 11:26:26 +0800

    680.验证回文字符串II

---
+++ 680.验证回文字符串II/example1.go
@@ -0,0 +1,28 @@
+package LeetCode680
+
+func validPalindrome(s string) bool {
+	low, high := 0, len(s)-1
+	for low < high {
+		if s[low] == s[high] {
+			low++
+			high--
+		} else {
+			flag1, flag2 := true, true
+			for i, j := low, high-1; i < j; i, j = i+1, j-1 {
+				if s[i] != s[j] {
+					flag1 = false
+					break
+				}
+			}
+
+			for i, j := low+1, high; i < j; i, j = i+1, j-1 {
+				if s[i] != s[j] {
+					flag2 = false
+					break
+				}
+			}
+			return flag1 || flag2
+		}
+	}
+	return true
+}
