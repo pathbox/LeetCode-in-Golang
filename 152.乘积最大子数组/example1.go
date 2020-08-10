31634e0873b752537ad2b9a6bbf52d0551512e18
pathbox <cary.luo@ucloud.cn>
2020-08-10 23:10:52 +0800

    162.寻找峰值

--- 152.乘积最大子数组/example1.go
+++ 152.乘积最大子数组/example1.go
@@ -34,3 +34,35 @@ func min(a, b int) int {
 
 	return b
 }
+
+/*
+func maxProduct(nums []int) int {
+    temp0, temp1, res := nums[0], nums[0], nums[0]
+	for i:=0; i<len(nums)-1; i++ {
+		if nums[i+1] >= 0 {
+			temp0, temp1 = max(temp0 * nums[i+1], nums[i+1]), min(temp1 * nums[i+1], nums[i+1])
+		} else {
+			temp0, temp1 = max(temp1 * nums[i+1], nums[i+1]), min(temp0 * nums[i+1], nums[i+1])
+		}
+		if res < temp0 {
+			res = temp0
+		}
+	}
+	return res
+}
+
+func max(a int, b int) int {
+	if a > b{
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a > b{
+		return b
+	}
+	return a
+}
+
+*/
---
+++ 162.寻找峰值/example1.go
@@ -0,0 +1,19 @@
+package LeetCode162
+
+func findPeakElement(nums []int) int {
+	var (
+		left, mid int
+		right     = len(nums) - 1
+	)
+
+	for left < right {
+		mid = (left + right) / 2
+		if nums[mid] > nums[mid+1] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
