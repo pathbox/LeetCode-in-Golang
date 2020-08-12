2f276d02ca24898780ba6e016cd7744a9a1f37ba
pathbox <cary.luo@ucloud.cn>
2020-08-12 21:03:09 +0800

    179.最大数

---
+++ 179.最大数/example1.go
@@ -0,0 +1,22 @@
+package LeetCode179
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func largestNumber(nums []int) string {
+	ss := make([]string, len(nums))
+	for i, num := range nums {
+		ss[i] = strconv.Itoa(num)
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i]+ss[j] >= ss[j]+ss[i] // 妙啊
+	})
+	o := strings.Join(ss, "")
+	if o[0] == '0' {
+		return "0"
+	}
+	return o
+}
