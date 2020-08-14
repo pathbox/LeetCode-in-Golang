35295f62387649566d2acffbcf083f8da08edcd7
pathbox <cary.luo@ucloud.cn>
2020-08-14 10:04:25 +0800

    221.最大正方形

--- 1277.统计全为1的正方形子矩阵/example1.go
+++ 1277.统计全为1的正方形子矩阵/example1.go
@@ -1,12 +1,13 @@
 package LeetCode1227
 
+// https://leetcode-cn.com/problems/count-square-submatrices-with-all-ones/solution/tong-ji-quan-wei-1-de-zheng-fang-xing-zi-ju-zhen-f/
 func countSquares(matrix [][]int) int {
 	max := 0
 	// 创建二维数组作为缓存
 	dp := make([][]int, len(matrix))
 	// 对二维数组进行赋值
 	for i := 0; i < len(matrix); i++ {
-		dp[1] = make([]int, len(matrix[i]))
+		dp[i] = make([]int, len(matrix[i]))
 		for j := 0; j < len(dp[i]); j++ {
 			dp[i][j] = matrix[i][j] - 0
 		}
---
+++ 221.最大正方形/example1.go
@@ -0,0 +1,36 @@
+package LeetCode221
+
+// https://leetcode-cn.com/problems/count-square-submatrices-with-all-ones/solution/tong-ji-quan-wei-1-de-zheng-fang-xing-zi-ju-zhen-f/
+
+func maximalSquare(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				maxSide = 1
+			}
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSide {
+					maxSide = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
