2f90e2e78ab969541a25582f296fe9b949b1fd46
pathbox <cary.luo@ucloud.cn>
2020-08-17 23:10:09 +0800

    461.汉明距离

---
+++ 461.汉明距离/example1.go
@@ -0,0 +1,30 @@
+package LeetCode461
+
+import "fmt"
+
+func hammingDistance(x int, y int) int {
+	C := x ^ y
+	count := 0
+
+	s := fmt.Sprintf("%b", C)
+	for i := range s {
+		if s[i] == 49 {
+			count++
+		}
+	}
+
+	return count
+}
+
+func hammingDistance(x int, y int) int {
+	i := x ^ y   //x和y异或,得到一个新的数(异或相同为0,不同为1,此时咱们需要统计1的个数)
+	count := 0   //定义数量的初始值为0
+	for i != 0 { //只要i不为0,那就继续循环
+		if (i & 1) == 1 { //如果i和1相与,值为1的话就count++
+			count++
+		}
+		i = i >> 1 //i右移一位
+	}
+	return count
+
+}
