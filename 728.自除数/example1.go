12a78def7a2c2ec653aeb8c00778bf85dbb352ac
pathbox <cary.luo@ucloud.cn>
2020-08-20 22:46:03 +0800

    728.自除数

---
+++ 728.自除数/example1.go
@@ -0,0 +1,26 @@
+package LeetCode728
+
+func isDividingNumbers(i int) bool {
+	if i < 10 {
+		return true
+	}
+	v := i
+	for i != 0 {
+		r := i % 10
+		if r == 0 || v%r != 0 {
+			return false
+		}
+		i = i / 10
+	}
+	return true
+}
+
+func selfDividingNumbers(left int, right int) []int {
+	var res []int
+	for i := left; i <= right; i++ {
+		if isDividingNumbers(i) {
+			res = append(res, i)
+		}
+	}
+	return res
+}
