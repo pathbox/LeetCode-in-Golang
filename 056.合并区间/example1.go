6aed180499f80de65a242c08a04198fb97780a48
pathbox <cary.luo@ucloud.cn>
2020-09-17 22:51:57 +0800

    056.合并区间

--- 056.合并区间/example1.go
+++ 056.合并区间/example1.go
@@ -2,6 +2,7 @@ package LeetCode056
 
 import "sort"
 
+// 上一个坐标的y坐标大于当前的x坐标并且y坐标小于当前y，表示可以合并 c[0]:x, c[1]:y
 func merge(intervals [][]int) [][]int {
 	ret := [][]int{}
 	sort.Slice(intervals, func(i, j int) bool {
@@ -13,7 +14,7 @@ func merge(intervals [][]int) [][]int {
 			ret = append(ret, s)
 			cs = s
 		} else if cs[1] >= s[0] && cs[1] < s[1] { // 能合并，just 扩展y坐标即可, cs[1] >= s[0]可以不写，因为上一个条件判断了cs[1] < s[0]操作
-			cs[1] = s[1]
+			cs[1] = s[1] // 能合并 改变c的y坐标
 		}
 	}
 	return ret
