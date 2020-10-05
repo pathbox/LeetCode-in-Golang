0021cf4074e729b59a72d36f2db06f46aba36935
pathbox <cary.luo@ucloud.cn>
2020-10-05 22:31:27 +0800

    add commit

--- 258.各位相加/example2.go
+++ 258.各位相加/example2.go
@@ -6,3 +6,15 @@ func addDigits(num int) int {
 	}
 	return num
 }
+
+func addDigits(num int) int {
+	for num >= 10 {
+		res := 0
+		for num > 0 {
+			res += num % 10
+			num = num / 10
+		}
+		num = res
+	}
+	return num
+}
--- 377.组合总和IV/377_example1.go
+++ 377.组合总和IV/377_example1.go
@@ -5,6 +5,18 @@ package LeetCode377
 dp[nums...] == 1
 dp[i] = dp[i] + dp[i - nums...]
 此题的状态转移方程为dp[i] = dp[i-nums[0]]+dp[i-nums[1]]+...dp[i-nums[len-1]],条件为i>=nums[j];
+/**
+     * 这里状态定义就是题目要求的，并不难，状态转移方程要动点脑子，也不难：
+     * 状态转移方程：dp[i]= dp[i - nums[0]] + dp[i - nums[1]] + dp[i - nums[2]] + ... （当 [] 里面的数 >= 0）
+     * 特别注意：dp[0] = 1，表示，如果那个硬币的面值刚刚好等于需要凑出的价值，这个就成为 1 种组合方案
+     * 再举一个具体的例子：nums=[1, 3, 4], target=7;
+     * dp[7] = dp[6] + dp[4] + dp[3]
+     * 即：7 的组合数可以由三部分组成，1 和 dp[6]，3 和 dp[4], 4 和dp[3];
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
 */
 func combinationSum4(nums []int, target int) int {
 	return DP(nums, target)
