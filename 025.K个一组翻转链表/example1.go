a8948a8ebd64ccab24778d413d5e1b27d03cf150
pathbox <cary.luo@ucloud.cn>
2020-10-06 17:21:04 +0800

    025.K个一组翻转链表

---
+++ 025.K个一组翻转链表/example1.go
@@ -0,0 +1,35 @@
+package LeetCode025
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return dummy.Next
+			}
+		}
+		next := tail.Next
+		head, tail = reverse(head, tail)
+		pre.Next = head
+		tail.Next = next
+		pre = tail
+		head = tail.Next
+	}
+	return dummy.Next
+}
+
+func reverse(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		next := p.Next
+		p.Next = prev
+		prev = p
+		p = next
+	}
+	return tail, head
+}
