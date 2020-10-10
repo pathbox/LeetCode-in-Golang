73cd30e45337afd11623cf7fc4502c8843b0069d
pathbox <cary.luo@ucloud.cn>
2020-10-10 08:56:13 +0800

    210.课程表II

---
+++ 210.课程表II/example1.go
@@ -0,0 +1,37 @@
+package LeetCode210
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	var (
+		edges  = make([][]int, numCourses)
+		indeg  = make([]int, numCourses)
+		result []int
+	)
+
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+		indeg[info[0]]++
+	}
+
+	q := []int{}
+	for i := 0; i < numCourses; i++ {
+		if indeg[i] == 0 {
+			q = append(q, i)
+		}
+	}
+
+	for len(q) > 0 {
+		u := q[0]
+		q = q[1:]
+		result = append(result, u)
+		for _, v := range edges[u] {
+			indeg[v]--
+			if indeg[v] == 0 {
+				q = append(q, v)
+			}
+		}
+	}
+	if len(result) != numCourses {
+		return []int{}
+	}
+	return result
+}
