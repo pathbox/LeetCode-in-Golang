55b7bc8329d3534372e0be5c4c2960b25dc05bf4
pathbox <cary.luo@ucloud.cn>
2020-11-04 22:14:01 +0800

    438.找到字符串中所有字母异位词example1

---
+++ 438.找到字符串中所有字母异位词/example1.go
@@ -0,0 +1,26 @@
+package LeetCode0438
+
+func findAnagrams(s string, p string) []int {
+	res := make([]int, 0)
+	lp := len(p)
+	ls := len(s)
+	if lp > ls {
+		return res
+	}
+	cnt1, cnt2 := [26]int{}, [26]int{}
+	for i := range p {
+		cnt1[p[i]-'a']++
+		cnt2[s[i]-'a']++
+	}
+
+	for i := 0; i <= ls-lp; i++ {
+		if cnt1 == cnt2 { // 两个窗口相等
+			res = append(res, i)
+		}
+		cnt2[s[i]-'a']--
+		if i+lp < ls {
+			cnt2[s[i+lp]-'a']++ // 窗口大小就是lp
+		}
+	}
+	return res
+}
