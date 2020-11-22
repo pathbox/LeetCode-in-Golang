937f243a5774c041b80bd877e93639a51927122d
pathbox <cary.luo@ucloud.cn>
2020-11-22 13:39:35 +0800

    1638.统计只差一个字符的子串数目:暴力枚举

---
+++ 1638.统计只差一个字符的子串数目/example2.go
@@ -0,0 +1,24 @@
+package LeetCode1638
+
+func countSubstrings(s, t string) int {
+	m := len(s)
+	n := len(t)
+	ans := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			diff := 0
+			for k := 0; i+k < m && j+k < n; k++ {
+				if s[i+k] != t[j+k] {
+					diff++
+				}
+				if diff > 1 {
+					break
+				}
+				if diff == 1 {
+					ans++
+				}
+			}
+		}
+	}
+	return ans
+}
