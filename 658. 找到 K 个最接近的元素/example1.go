cc74ca8c7c71ffa2d05837604194ca4456974dd6
pathbox <cary.luo@ucloud.cn>
2021-01-12 09:52:35 +0800

    658. 找到 K 个最接近的元素:双指针

---
+++ 658. 找到 K 个最接近的元素/example1.go
@@ -0,0 +1,16 @@
+package LeetCode658
+
+func findClosestElements(arr []int, k int, x int) []int {
+	start := 0
+	end := len(arr) - 1
+
+	removeNums := len(arr) - k
+	for i := 0; i < removeNums; i++ {
+		if x-arr[start] <= arr[end]-x {
+			end--
+		} else {
+			start++
+		}
+	}
+	return arr[start : end+1]
+}
