e75d335318ace5dab0029cb5dbfbe2c64188e68c
pathbox <cary.luo@ucloud.cn>
2021-01-17 13:05:22 +0800

    405.数字转换为十六进制数

--- 786.第K个最小的素数分数/example1.go
+++ 786.第K个最小的素数分数/example1.go
@@ -7,7 +7,7 @@ func kthSmallestPrimeFraction(arr []int, k int) []int {
 	for high-low > 1e-9 {
 		mid := low + (high-low)/2.0
 		res := under(mid, arr)
-		if res[0] < k {
+		if res[0] < k { // 说明数量太少
 			low = mid
 		} else {
 			ans[0] = res[1]
@@ -21,7 +21,7 @@ func kthSmallestPrimeFraction(arr []int, k int) []int {
 func under(x float64, arr []int) []int {
 	numer, denom, count, i := 0, 1, 0, -1
 	for j := 1; j < len(arr); j++ {
-		for float64(arr[i+1]) < float64(arr[j])*x {
+		for float64(arr[i+1]) < float64(arr[j])*x { // primes[i] / primes[j] < x
 			i++
 		}
 		count += i + 1
@@ -32,3 +32,9 @@ func under(x float64, arr []int) []int {
 	}
 	return []int{count, numer, denom}
 }
+
+/*
+时间复杂度：O(N \log W)O(NlogW)，其中 NN 是 primes 的长度，W 是二分查找的区间宽度，(hi - lo) / 1e-9 等于 10^9
+
+空间复杂度：O(1)
+*/
