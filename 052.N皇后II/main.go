79f86955fcf786256e2a0dfcabe9ec8980b51f8d
pathbox <cary.luo@ucloud.cn>
2021-01-22 10:03:04 +0800

    052.N皇后II:example1

---
+++ 052.N皇后II/main.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+var res int
+
+func main() {
+	n := 20
+	fmt.Println(totalNQueens(n))
+}
+func totalNQueens(n int) int {
+	var tmp = make([][]rune, n)
+	for r := 0; r < n; r++ {
+		for c := 0; c < n; c++ {
+			tmp[r] = append(tmp[r], '.')
+		}
+	}
+
+	backTrack(0, tmp)
+	return res
+}
+
+func backTrack(row int, tmp [][]rune) {
+	if row == len(tmp) {
+		res++
+		return
+	}
+
+	for c := 0; c < len(tmp); c++ {
+		if !valid(row, c, tmp) {
+			continue
+		}
+		tmp[row][c] = 'Q'
+		backTrack(row+1, tmp)
+		tmp[row][c] = '.'
+	}
+}
+
+func valid(row, col int, tmp [][]rune) bool {
+	for i := 0; i < len(tmp); i++ {
+		if tmp[row][i] == 'Q' {
+			return false
+		}
+	}
+
+	for i := 0; i < len(tmp); i++ {
+		if tmp[i][col] == 'Q' {
+			return false
+		}
+	}
+
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if tmp[i][j] == 'Q' {
+			return false
+		}
+	}
+
+	for i, j := row-1, col+1; i >= 0 && j < len(tmp); i, j = i-1, j+1 {
+		if tmp[i][j] == 'Q' {
+			return false
+		}
+	}
+
+	return true
+}
--- 51.N皇后/example1.go
+++ 51.N皇后/example1.go
@@ -1,5 +1,3 @@
-package LeetCode51
-
 func solveNQueens(n int) [][]string {
 	var tmp = make([][]rune, n)
 	for r := 0; r < n; r++ {
@@ -7,7 +5,6 @@ func solveNQueens(n int) [][]string {
 			tmp[r] = append(tmp[r], '.')
 		}
 	}
-
 	var res [][]string
 	backTrack(0, tmp, &res)
 	return res
@@ -17,7 +14,7 @@ func backTrack(row int, tmp [][]rune, res *[][]string) {
 	if row == len(tmp) {
 		var subRes []string
 		for i := 0; i < row; i++ {
-			subRes = append(subRes, string(tmp[i]))
+			subRes = append(subRes, string(tmp[i])) // copy一个结果，不直接使用tmp
 		}
 		*res = append(*res, subRes)
 		return
@@ -39,20 +36,19 @@ func valid(row, col int, res [][]rune) bool {
 			return false
 		}
 	}
-
-	for i := 0; i < row; i++ {
-		if res[i][col] == 'Q' {
+	for j := 0; j < row; j++ {
+		if res[j][col] == 'Q' {
 			return false
 		}
 	}
-
+	// 3.右下角至左上角检测 以(row,col)为端点，检测左上部分，右下部分不用,因为现在只遍历到row行，下面的row都还没有填Q，因为下一行会继续往上检测
 	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
 		if res[i][j] == 'Q' {
 			return false
 		}
 	}
 
-	for i, j := row-1, col+1; i >= 0 && j <= len(res); i, j = i-1, j+1 {
+	for i, j := row-1, col+1; i >= 0 && j < len(res); i, j = i-1, j+1 {
 		if res[i][j] == 'Q' {
 			return false
 		}
