ceb2de25036d357abadd537fa5c0d933d7b95b6d
pathbox <cary.luo@ucloud.cn>
2021-04-04 19:48:46 +0800

    714.买卖股票的最佳时机含手续费

--- 714.买卖股票的最佳时机含手续费/example1.go
+++ 714.买卖股票的最佳时机含手续费/example1.go
@@ -1,14 +1,15 @@
-package LeetCode122
+package LeetCode714
 
-func maxProfit(prices []int) int {
+func maxProfit(prices []int, fee int) int {
 	if len(prices) < 2 {
 		return 0
 	}
 	dp := make([][2]int, len(prices))
 	dp[0][0] = 0
-	dp[0][1] = -prices[0] // 第一天买入股票
+	dp[0][1] = -prices[0]
+
 	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]-fee)
 		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
 	}
 	return dp[len(prices)-1][0]
