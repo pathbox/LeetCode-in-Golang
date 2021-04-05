9b6bd612cc9e9dd8b3d8c89fae6ba9caf7c1140f
pathbox <cary.luo@ucloud.cn>
2021-04-05 17:15:38 +0800

    1052. 爱生气的书店老板:滑动窗口

---
+++ 1052. 爱生气的书店老板/example.go
@@ -0,0 +1,30 @@
+package LeetCode1052
+
+func maxSatisfied(customers []int, grumpy []int, X int) int {
+	// 计算X天能够得到的最大额外收益
+	total := 0 // total 是不使用技能时候的正常收益
+	n := len(customers)
+	for i := 0; i < n; i++ {
+		if grumpy[i] == 0 {
+			total += customers[i]
+		}
+	}
+
+	increase := 0
+	for i := 0; i < X; i++ {
+		increase += customers[i] * grumpy[i]
+	}
+	maxIncrease := increase
+	for i := X; i < n; i++ {
+		increase = increase - customers[i-X]*grumpy[i-X] + customers[i]*grumpy[i] // i - X 其实是左指针位置， i是右指针
+		maxIncrease = max(maxIncrease, increase)
+	}
+	return total + maxIncrease
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
